ce6a17994e937c8bf576f8e2ab442382fba7738a
Christopher Chamberlain <chamberlain@live.ca>
2019-09-09 19:00:04 -0600

    Added music example

---
+++ Examples/Music Player/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+using Heirloom.IO;
+using Heirloom.Sound;
+
+namespace Examples.MusicPlayer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var stream = Files.OpenStream("files/wholesome-by-kevin-macleod.mp3"))
+            {
+                // Construct a new streaming audio source
+                var source = new AudioSource(stream);
+                source.Play();
+
+                // Write Attributution (because CC4)
+                Console.WriteLine("Music from https://filmmusic.io");
+                Console.WriteLine("\"Wholesome\" by Kevin MacLeod(https://incompetech.com)");
+                Console.WriteLine("License: CC BY(http://creativecommons.org/licenses/by/4.0/)");
+                Console.WriteLine("\nPlaying music!");
+
+                // Block until song is complete
+                var finished = false;
+                source.PlaybackEnded += () => finished = true;
+                SpinWait.SpinUntil(() => finished);
+
+                Console.WriteLine("Thanks for listening!");
+            }
+        }
+    }
+}
