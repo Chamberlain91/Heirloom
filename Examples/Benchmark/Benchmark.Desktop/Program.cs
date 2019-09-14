f0c70cff40059391f3db405f144fee301da868bf
Christopher Chamberlain <chamberlain@live.ca>
2019-09-14 01:06:59 -0600

    Added Benchmark back in, incomplete android

---
+++ Examples/Benchmark/Benchmark.Desktop/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Heirloom.Desktop;
+using Heirloom.Drawing;
+using Heirloom.IO;
+
+namespace Benchmark
+{
+    internal class Program : GameWindow
+    {
+        public BenchmarkApp App;
+
+        public Program()
+            : base("Heirloom Benchmark", vsync: false)
+        {
+            Maximize();
+
+            // Display FPS
+            ShowFPSOverlay = true;
+
+            foreach (var ef in Files.GetEmbeddedFiles())
+            {
+                Console.WriteLine(ef.Path);
+                foreach (var id in ef.Identifiers)
+                {
+                    Console.WriteLine("    " + id);
+                }
+            }
+
+            // 
+            App = new BenchmarkApp(60, 8, 20000);
+        }
+
+        protected override void Update(float dt)
+        {
+            App.Update(dt);
+        }
+
+        protected override void Draw(RenderContext ctx, float dt)
+        {
+            App.Render(ctx, dt);
+        }
+
+        private static void Main(string[] args)
+        {
+            Application.Run(() => new Program());
+        }
+    }
+}
---
+++ Examples/Benchmark/Benchmark/Particle.cs
@@ -0,0 +1,54 @@
+﻿using Heirloom.Drawing;
+using Heirloom.Math;
+
+namespace Benchmark
+{
+    public class Particle
+    {
+        public Image Image { get; }
+
+        public Vector Position;
+
+        public Vector Velocity;
+
+        public Matrix Transform => Matrix.CreateTranslation(Position);
+
+        public Particle(Image image)
+        {
+            Image = image;
+        }
+
+        public void Update(in Rectangle bounds, in float delta, in float scale)
+        {
+            Position += Velocity * delta;
+
+            // Off right edge
+            if (Position.X + Image.Width * scale > bounds.Right && Velocity.X > 0)
+            {
+                Velocity.X *= -1;
+                Position.X = bounds.Right - Image.Width * scale;
+            }
+
+            // Off left edge
+            if (Position.X < bounds.Left && Velocity.X < 0)
+            {
+                Velocity.X *= -1;
+                Position.X = bounds.Left;
+            }
+
+            // Off bottom
+            if (Position.Y + Image.Height * scale > bounds.Bottom && Velocity.Y > 0)
+            {
+                Velocity.Y *= -1;
+                Position.Y = bounds.Bottom - Image.Height * scale;
+            }
+
+            // Off top
+            if (Position.Y < bounds.Top && Velocity.Y < 0)
+            {
+                Velocity.Y *= -1;
+                Position.Y = bounds.Top;
+            }
+        }
+    }
+}
