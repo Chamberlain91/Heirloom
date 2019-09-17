e4ad3e8d7c7b78446def784750bda85f46aaf7f1
Christopher Chamberlain <chamberlain@live.ca>
2019-09-16 22:58:49 -0600

    Modified Minimal to say 'Hello <OS>!'

--- Examples/Desktop/Examples.Desktop.CustomText/Program.cs
+++ Examples/Desktop/Examples.Desktop.CustomText/Program.cs
@@ -1,20 +1,17 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics;
-
-using Heirloom.Desktop;
+﻿using Heirloom.Desktop;
 using Heirloom.Drawing;
-using Heirloom.Math;
 
 namespace Examples.CustomText
 {
     internal class Program : GameWindow
     {
-        public RichText Text = new RichText("You have found the [Blade of Harmony]!");
+        private readonly CustomTextExample _app;
 
         public Program()
             : base("Custom Text Callback")
         {
             ShowFPSOverlay = true;
+            _app = new CustomTextExample();
         }
 
         private static void Main(string[] args)
@@ -24,73 +21,12 @@ namespace Examples.CustomText
 
         protected override void Update(float dt)
         {
-            // Nothing To Do
+            _app.Update(dt);
         }
 
         protected override void Draw(RenderContext ctx, float dt)
         {
-            ctx.Clear(Color.DarkGray);
-
-            // 
-            var position = new Vector(ctx.Surface.Width / 2F, ctx.Surface.Height - 128);
-            Text.Draw(ctx, position, Font.Default, 64, TextAlign.Center);
-        }
-
-        public class RichText
-        {
-            private readonly Stopwatch _stopwatch;
-            private readonly bool[] _states;
-
-            public RichText(string text)
-            {
-                RawText = text;
-
-                _stopwatch = Stopwatch.StartNew();
-
-                // Process Text
-                var states = new List<bool>();
-                var chars = new List<char>();
-
-                var isSpecial = false;
-                for (var i = 0; i < text.Length; i++)
-                {
-                    var ch = text[i];
-
-                    // Found special bound
-                    if (ch == '[') { isSpecial = true; continue; }
-                    if (ch == ']') { isSpecial = false; continue; }
-
-                    // Append current character and state
-                    states.Add(isSpecial);
-                    chars.Add(ch);
-                }
-
-                // 
-                Text = new string(chars.ToArray());
-                _states = states.ToArray();
-            }
-
-            public string RawText { get; }
-
-            public string Text { get; }
-
-            internal void CallbackProcessor(string text, int index, ref CharacterRenderState state)
-            {
-                ref var isSpecial = ref _states[index];
-                state.Color = isSpecial ? Color.Violet : Color.White;
-
-                // 
-                if (isSpecial)
-                {
-                    var now = (float) _stopwatch.Elapsed.TotalSeconds;
-                    state.Position.Y += Calc.Sin(index / 4F + now * 4F) * 3;
-                }
-            }
-
-            public void Draw(RenderContext ctx, Vector position, Font font, int size, TextAlign align)
-            {
-                ctx.DrawText(Text, position, font, size, align, CallbackProcessor);
-            }
+            _app.Draw(ctx, dt);
         }
     }
 }
