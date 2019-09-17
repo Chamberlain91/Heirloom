d5da6ad42d6e432b40ea652aa535aeb79b50309f
Christopher Chamberlain <chamberlain@live.ca>
2019-09-17 02:50:11 -0600

    Added some minor documentation, more to go

--- Modules/Heirloom.Drawing/Axis.cs
+++ Modules/Heirloom.Drawing/Axis.cs
@@ -1,8 +1,18 @@
 ﻿namespace Heirloom.Drawing
 {
+    /// <summary>
+    /// Represents an axis of the 2D plane.
+    /// </summary>
     public enum Axis
     {
+        /// <summary>
+        /// The horizontal or X axis.
+        /// </summary>
         Horizontal,
+
+        /// <summary>
+        /// The vertical or Y axis.
+        /// </summary>
         Vertical
     }
 }
