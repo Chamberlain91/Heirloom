6e199bb6f9225898c775d64cb7c76c4665c5f2e7
Christopher Chamberlain <chamberlain@live.ca>
2019-09-16 22:58:49 -0600

    Modified Minimal to say 'Hello <OS>!'

--- Examples/Desktop/Examples.Desktop.Minimal/Program.cs
+++ Examples/Desktop/Examples.Desktop.Minimal/Program.cs
@@ -1,4 +1,6 @@
-﻿using Heirloom.Desktop;
+﻿using System.Runtime.InteropServices;
+
+using Heirloom.Desktop;
 using Heirloom.Drawing;
 using Heirloom.Math;
 
@@ -8,7 +10,8 @@ namespace Examples.Minimal
     {
         private static void Main(string[] args)
         {
-            const string Text = "Hello World!";
+            var text = $"Hello {GetOperatingSystem()}!";
+            var fontSize = 48;
 
             Application.Run(() =>
             {
@@ -16,17 +19,28 @@ namespace Examples.Minimal
                 var window = new Window(500, 300, "Minimal Example");
                 window.IsResizable = false;
 
+                // 
                 var ctx = window.RenderContext;
 
                 // Draw hello world text
+                ctx.ResetState(); // bug: should not need to be called, but clear color doesn't show?!
                 ctx.Clear(Color.DarkGray);
 
-                // todo: feature, should be able to vertical align without this.
-                var align = new Vector(0, Font.Default.MeasureText(Text, 48).Height / 2F);
-                ctx.DrawText(Text, -align + ((Vector) window.FramebufferSize) * 0.5F, Font.Default, 48, TextAlign.Center);
+                // todo: Implement feature, should be able to vertical align text without this step.
+                var align = new Vector(0, Font.Default.MeasureText(text, fontSize).Height / 2F);
+                ctx.DrawText(text, -align + ((Vector) window.FramebufferSize) * 0.5F, Font.Default, fontSize, TextAlign.Center);
 
+                // Put drawn image on screen
                 ctx.SwapBuffers();
             });
+        }
+
+        private static string GetOperatingSystem()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) { return "Windows"; }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) { return "macOS"; }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) { return "Linux"; }
+            else { return "Computer?"; }
         }
     }
 }
