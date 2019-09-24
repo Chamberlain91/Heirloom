8c4ddb9a06b8846adb0c06f269ce4131ad23fe7a
Christopher Chamberlain <chamberlain@live.ca>
2019-09-24 00:50:07 -0600

    Removed Runtime and Input

--- Modules/Heirloom.Runtime/ICollisionCallback.cs
+++
@@ -1,9 +0,0 @@
-﻿using Heirloom.Math;
-
-namespace Heirloom.Runtime
-{
-    public interface ICollisionCallback
-    {
-        void OnCollision(Collider a, Collider b, in Manifold manifold);
-    }
-}
