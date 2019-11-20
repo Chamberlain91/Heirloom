e8b507010a95f6521dbd3e9039ea700f85964de9
Christopher Chamberlain <chamberlain@live.ca>
2019-11-20 01:42:54 -0600

    Added better warning when improperly using Application.Run

--- Platforms/Heirloom.Desktop/Application.cs
+++ Platforms/Heirloom.Desktop/Application.cs
@@ -16,6 +16,8 @@ namespace Heirloom.Desktop
         private static ConsumerQueue _invokeQueue;
         private static List<Window> _windows;
 
+        private static readonly object _lock = new object();
+
         /// <summary>
         /// Are transparent framebuffers supported on this platform?
         /// </summary>
@@ -23,43 +25,47 @@ namespace Heirloom.Desktop
 
         public static void Run(Action initialize)
         {
-            // Initialize GLFW
-            if (!Glfw.Initialize())
+            lock (_lock)
             {
-                Console.WriteLine("Unable to initialize GLFW");
-                return;
+                // Initialize GLFW
+                if (!Glfw.Initialize())
+                {
+                    Console.WriteLine("Unable to initialize GLFW");
+                    return;
+                }
+
+                // Set to use OpenGL 3.2 core (forward compatible)
+                Glfw.SetWindowCreationHint(WindowAttribute.ClientApi, (int) ClientApi.OpenGL);
+                Glfw.SetWindowCreationHint(WindowAttribute.OpenGLProfile, (int) OpenGLProfile.Core);
+                Glfw.SetWindowCreationHint(WindowAttribute.OpenGLForwardCompatibility, true);
+                Glfw.SetWindowCreationHint(WindowAttribute.ContextVersionMajor, 3);
+                Glfw.SetWindowCreationHint(WindowAttribute.ContextVersionMinor, 2);
+
+                // Create "dummy" window, the GL context sharing window
+                Glfw.SetWindowCreationHint(WindowAttribute.Visible, false);
+                Glfw.SetWindowCreationHint(WindowAttribute.TransparentFramebuffer, true);
+                ShareContext = Glfw.CreateWindow(256, 256, "GLFW Background Window");
+
+                // Determine if transparent framebuffers are possible
+                SupportsTransparentFramebuffer = Glfw.GetWindowAttribute(ShareContext, WindowAttribute.TransparentFramebuffer) != 0;
+
+                // Set default window creation hints
+                Glfw.SetWindowCreationHint(WindowAttribute.FocusOnShow, true);
+                Glfw.SetWindowCreationHint(WindowAttribute.Visible, true);
+                // Bind share context and load GL functions
+                Glfw.MakeContextCurrent(ShareContext);
+                GL.LoadFunctions(Glfw.GetProcAddress);
+
+                // Release context for use as a sharing context.
+                Glfw.MakeContextCurrent(WindowHandle.None);
+
+                // 
+                _invokeQueue = new ConsumerQueue();
+                _windows = new List<Window>();
             }
 
-            // Set to use OpenGL 3.2 core (forward compatible)
-            Glfw.SetWindowCreationHint(WindowAttribute.ClientApi, (int) ClientApi.OpenGL);
-            Glfw.SetWindowCreationHint(WindowAttribute.OpenGLProfile, (int) OpenGLProfile.Core);
-            Glfw.SetWindowCreationHint(WindowAttribute.OpenGLForwardCompatibility, true);
-            Glfw.SetWindowCreationHint(WindowAttribute.ContextVersionMajor, 3);
-            Glfw.SetWindowCreationHint(WindowAttribute.ContextVersionMinor, 2);
-
-            // Create "dummy" window, the GL context sharing window
-            Glfw.SetWindowCreationHint(WindowAttribute.Visible, false);
-            Glfw.SetWindowCreationHint(WindowAttribute.TransparentFramebuffer, true);
-            ShareContext = Glfw.CreateWindow(256, 256, "GLFW Background Window");
-
-            // Determine if transparent framebuffers are possible
-            SupportsTransparentFramebuffer = Glfw.GetWindowAttribute(ShareContext, WindowAttribute.TransparentFramebuffer) != 0;
-
-            // Set default window creation hints
-            Glfw.SetWindowCreationHint(WindowAttribute.FocusOnShow, true);
-            Glfw.SetWindowCreationHint(WindowAttribute.Visible, true);
-            // Bind share context and load GL functions
-            Glfw.MakeContextCurrent(ShareContext);
-            GL.LoadFunctions(Glfw.GetProcAddress);
-
-            // Release context for use as a sharing context.
-            Glfw.MakeContextCurrent(WindowHandle.None);
-
             // 
-            _invokeQueue = new ConsumerQueue();
-            _windows = new List<Window>();
-
-            // 
+            IsInitialized = true;
             initialize();
 
             // While any window is open
@@ -79,13 +85,25 @@ namespace Heirloom.Desktop
             Glfw.Terminate();
         }
 
-        public static bool IsInitialized { get; private set; }
+        /// <summary>
+        /// Gets a value determining if the application is ready to handle windows and other features.
+        /// </summary>
+        public static bool IsInitialized { get; private set; } = false;
 
-        public static IReadOnlyList<Window> Windows => _windows;
+        public static IReadOnlyList<Window> Windows
+        {
+            get
+            {
+                EnsureReady();
+                return _windows;
+            }
+        }
 
         internal static void AddWindow(Window window)
         {
-            lock (_windows)
+            EnsureReady();
+
+            lock (_lock)
             {
                 _windows.Add(window);
             }
@@ -93,7 +111,9 @@ namespace Heirloom.Desktop
 
         internal static void RemoveWindow(Window window)
         {
-            lock (_windows)
+            EnsureReady();
+
+            lock (_lock)
             {
                 _windows.Remove(window);
             }
@@ -114,6 +134,14 @@ namespace Heirloom.Desktop
         public static T Invoke<T>(Func<T> action)
         {
             return _invokeQueue.Invoke(action);
+        }
+
+        private static void EnsureReady()
+        {
+            if (IsInitialized == false)
+            {
+                throw new InvalidOperationException($"You must execute the application via {nameof(Application)}.{nameof(Run)}.");
+            }
         }
     }
 }
