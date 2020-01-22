46e363bd42ab86f4b5ab2180502a9962bf868a73
Chris Chamberlain <chamberlain@live.ca>
2020-01-22 00:16:29 -0600

    WIP: Trying to figure out a cross platform machine info for benchmark data

---
+++ Examples/Heirloom.Benchmark/ProcessorInfo.cs
@@ -0,0 +1,20 @@
+﻿namespace Heirloom.Benchmark
+{
+    public readonly struct ProcessorInfo
+    {
+        public readonly string Name;
+     
+        public readonly string ClockSpeed;
+        
+        public readonly int CoreCount;
+
+        public ProcessorInfo(string name, string clockSpeed, int coreCount)
+        {
+            Name = name;
+            ClockSpeed = clockSpeed;
+            CoreCount = coreCount;
+        }
+
+        public static ProcessorInfo Unknown { get; } = new ProcessorInfo("Unknown", "Unknown", -1);
+    }
+}
---
+++ Examples/Heirloom.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Heirloom.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
