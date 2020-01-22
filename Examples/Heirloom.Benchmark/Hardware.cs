001b9055a45ad2f567d7853850fb14132ba3ba68
Christopher Chamberlain <chamberlain@live.ca>
2020-01-22 15:14:08 -0600

    Implemented windows processor info via wmic

--- Examples/Heirloom.Benchmark/Hardware.cs
+++ Examples/Heirloom.Benchmark/Hardware.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace Heirloom.Benchmark
 {
@@ -8,26 +10,88 @@ namespace Heirloom.Benchmark
         {
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                return GetWindowsProcessorName();
+                // windows 10, windows 7, etc
+                return GetWindowsProcessorInfo();
             }
             else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
-                return GetLinuxProcessorName();
+                // linux, ubuntu, etc
+                return GetLinuxProcessorInfo();
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                // macOS
+                return GetMacProcessorInfo();
             }
             else
             {
+                // No clue what platform we are on
                 return ProcessorInfo.Unknown;
             }
         }
 
-        private static ProcessorInfo GetWindowsProcessorName()
+        private static ProcessorInfo GetWindowsProcessorInfo()
+        {
+            // 
+            var wmicInfo = QueryProcess("wmic", "cpu get name, maxclockspeed, numberoflogicalprocessors /format:list").Split('\n');
+
+            var clockSpeed = ProcessorInfo.Unknown.ClockSpeed;
+            var coreCount = ProcessorInfo.Unknown.ProcessorCount;
+            var name = ProcessorInfo.Unknown.Name;
+
+            foreach (var line in wmicInfo)
+            {
+                var parts = line.Split('=');
+                switch (parts[0].ToLower())
+                {
+                    case "numberoflogicalprocessors":
+                        coreCount = int.Parse(parts[1]);
+                        break;
+
+                    case "maxclockspeed":
+                        clockSpeed = int.Parse(parts[1]);
+                        break;
+
+                    case "name":
+                        name = parts[1];
+                        break;
+
+                }
+            }
+
+            // Return processor information
+            return new ProcessorInfo(name, clockSpeed, coreCount);
+        }
+
+        private static ProcessorInfo GetLinuxProcessorInfo()
         {
             return ProcessorInfo.Unknown;
         }
 
-        private static ProcessorInfo GetLinuxProcessorName()
+        private static ProcessorInfo GetMacProcessorInfo()
         {
+            // shell -> sysctl - n machdep.cpu.brand_string
+            // "Intel(R) Core(TM) i7 - 3770S CPU @ 3.10GHz"
             return ProcessorInfo.Unknown;
+        }
+
+        private static string QueryProcess(string exe, string args)
+        {
+            var p = new Process();
+
+            // Configure process start info
+            p.StartInfo.FileName = exe;   // ie, "ls"
+            p.StartInfo.Arguments = args; // ie, "-a"
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.UseShellExecute = false;
+
+            // Launch process
+            p.Start();
+
+            // Wait for all output
+            var output = p.StandardOutput.ReadToEnd().Trim();
+            p.WaitForExit();
+            return output;
         }
     }
 }
--- Examples/Heirloom.Benchmark/ProcessorInfo.cs
+++ Examples/Heirloom.Benchmark/ProcessorInfo.cs
@@ -1,20 +1,70 @@
-﻿namespace Heirloom.Benchmark
-{
-    public readonly struct ProcessorInfo
-    {
-        public readonly string Name;
-     
-        public readonly string ClockSpeed;
-        
-        public readonly int CoreCount;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
-        public ProcessorInfo(string name, string clockSpeed, int coreCount)
+namespace Heirloom.Benchmark
+{
+    public readonly struct ProcessorInfo : IEquatable<ProcessorInfo>
+    {
+        /// <summary>
+        /// Gets the name of the processor on this system.
+        /// </summary>
+        public readonly string Name;
+
+        /// <summary>
+        /// Gets the clock speed (in megahertz) on this system.
+        /// </summary>
+        public readonly int ClockSpeed;
+
+        /// <summary>
+        /// The number of logical processors on this system.
+        /// </summary>
+        public readonly int ProcessorCount;
+
+        /// <summary>
+        /// Gets the default information when properties of CPU are unknown.
+        /// </summary>
+        public static ProcessorInfo Unknown { get; } = new ProcessorInfo("Unknown", -1, -1);
+
+        #region Constructors
+
+        public ProcessorInfo(string name, int clockSpeed, int coreCount)
         {
             Name = name;
             ClockSpeed = clockSpeed;
-            CoreCount = coreCount;
+            ProcessorCount = coreCount;
         }
 
-        public static ProcessorInfo Unknown { get; } = new ProcessorInfo("Unknown", "Unknown", -1);
+        #endregion
+
+        #region Equality
+
+        public override bool Equals(object obj)
+        {
+            return obj is ProcessorInfo info && Equals(info);
+        }
+
+        public bool Equals([AllowNull] ProcessorInfo other)
+        {
+            return Name == other.Name &&
+                   ClockSpeed == other.ClockSpeed &&
+                   ProcessorCount == other.ProcessorCount;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, ClockSpeed, ProcessorCount);
+        }
+
+        public static bool operator ==(ProcessorInfo left, ProcessorInfo right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(ProcessorInfo left, ProcessorInfo right)
+        {
+            return !(left == right);
+        }
+
+        #endregion
     }
 }
