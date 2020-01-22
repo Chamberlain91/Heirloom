55543c5b02eec86f68318785d72cae3fdaf6ae00
Chris Chamberlain <chamberlain@live.ca>
2020-01-22 15:14:08 -0600

    Implemented windows processor info via wmic

--- Examples/Heirloom.Benchmark/Program.cs
+++ Examples/Heirloom.Benchmark/Program.cs
@@ -6,7 +6,9 @@ namespace Heirloom.Benchmark
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var info = Hardware.GetProcessorInfo();
+            Console.WriteLine($"Name: {info.Name}");
+            Console.WriteLine($"{info.ProcessorCount} Logical Processors @ {info.ClockSpeed / 1000:N1}GHz");
         }
     }
 }
