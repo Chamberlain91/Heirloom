4ccb5559b84eae741bf84fbb6f4743bf8dab45da
Chris Chamberlain <chamberlain@live.ca>
2020-01-27 02:52:27 -0600

    Added emote icons

---
+++ Examples/Heirloom.Benchmark/Benchmarks/AdventureBenchmark.cs
@@ -0,0 +1,9 @@
+﻿namespace Heirloom.Benchmark
+{
+    public sealed class AdventureBenchmark : ParticleBenchmark
+    {
+        public AdventureBenchmark()
+            : base("Adventure Sprites", Assets.LoadImages("files/adventure", true))
+        { }
+    }
+}
