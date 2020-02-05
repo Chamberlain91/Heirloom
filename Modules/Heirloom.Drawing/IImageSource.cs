945ebbd7b8e379906cc36c820f2a4f7afeddbc85
Christopher Chamberlain <chamberlain@live.ca>
2020-02-05 12:09:29 -0600

    WIP: Mockup of Image/Surface 2.0

---
+++ Modules/Heirloom.Drawing/IImageSource.cs
@@ -0,0 +1,15 @@
+﻿using Heirloom.Math;
+
+namespace Heirloom.Drawing
+{
+    internal interface IImageSource
+    {
+        IntSize Size { get; }
+
+        Vector Origin { get; set; }
+
+        InterpolationMode InterpolationMode { get; set; }
+
+        uint Version { get; set; }
+    }
+}
