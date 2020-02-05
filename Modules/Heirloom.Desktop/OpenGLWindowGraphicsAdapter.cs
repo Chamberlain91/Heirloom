1807b2f60be365b9dec2b669b56cb3010ddf0232
Christopher Chamberlain <chamberlain@live.ca>
2020-02-05 11:57:16 -0600

    Invoking via GL adapter tries to select GL thread before window/app thread

--- Modules/Heirloom.Desktop/OpenGLWindowGraphicsAdapter.cs
+++ Modules/Heirloom.Desktop/OpenGLWindowGraphicsAdapter.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Threading;
 
 using Heirloom.Drawing;
@@ -8,55 +10,80 @@ namespace Heirloom.Desktop
 {
     internal sealed class OpenGLWindowGraphicsAdapter : OpenGLGraphicsAdapter, IWindowGraphicsFactory
     {
+        public OpenGLWindowGraphicsAdapter()
+        {
+            Graphics = new List<OpenGLWindowGraphics>();
+        }
+
+        private List<OpenGLWindowGraphics> Graphics { get; }
+
         public Graphics CreateGraphics(Window window, bool vsync)
         {
             return new OpenGLWindowGraphics(this, window, vsync);
         }
 
+        #region Invoke
+
         protected override T Invoke<T>(Func<T> function)
         {
-            return Application.Invoke(() =>
+            // If a graphics context is known, use that thread.
+            if (Graphics.Count > 0) { return Graphics[0].Invoke(function); }
+            // Otherwise, temporarily bind the shared context on the window thread.
+            else
             {
-                // Make the share context current here
-                // todo: Possibly correct for this, this feels terrible...
-                Glfw.MakeContextCurrent(Application.ShareContext);
+                return Application.Invoke(() =>
+                {
+                    // Make the share context current here
+                    // todo: Possibly correct for this, this feels terrible...
+                    Glfw.MakeContextCurrent(Application.ShareContext);
 
-                // Execute function and keep return value
-                var returnValue = function();
+                    // Execute function and keep return value
+                    var returnValue = function();
 
-                // Release context from thread. We want it not associated with any thread. On a AMD Vega
-                // platform this caused the main rendering loop to halt (resource blocking?).
-                Glfw.MakeContextCurrent(WindowHandle.None);
+                    // Release context from thread. We want it not associated with any thread. On a AMD Vega
+                    // platform this caused the main rendering loop to halt (resource blocking?).
+                    Glfw.MakeContextCurrent(WindowHandle.None);
 
-                return returnValue;
-            });
+                    return returnValue;
+                });
+            }
         }
 
         protected override void Invoke(Action function)
         {
-            Application.Invoke(() =>
+            // If a graphics context is known, use that thread.
+            if (Graphics.Count > 0) { Graphics[0].Invoke(function); }
+            // Otherwise, temporarily bind the shared context on the window thread.
+            else
             {
-                // Make the share context current here
-                // todo: Possibly correct for this, this feels terrible...
-                Glfw.MakeContextCurrent(Application.ShareContext);
+                Application.Invoke(() =>
+                {
+                    // Make the share context current here
+                    // todo: Possibly correct for this, this feels terrible...
+                    Glfw.MakeContextCurrent(Application.ShareContext);
 
-                // Execute function and keep return value
-                function();
+                    // Execute function and keep return value
+                    function();
 
-                // Release context from thread. We want it not associated with any thread. On a AMD Vega
-                // platform this caused the main rendering loop to halt (resource blocking?).
-                Glfw.MakeContextCurrent(WindowHandle.None);
-            });
+                    // Release context from thread. We want it not associated with any thread. On a AMD Vega
+                    // platform this caused the main rendering loop to halt (resource blocking?).
+                    Glfw.MakeContextCurrent(WindowHandle.None);
+                });
+            }
         }
+
+        #endregion
 
         private sealed class OpenGLWindowGraphics : OpenGLGraphics
         {
+            private readonly OpenGLWindowGraphicsAdapter _adapter;
             private readonly Window _window;
             private readonly bool _vsync;
 
-            public OpenGLWindowGraphics(GraphicsAdapter adapter, Window window, bool vsync)
+            public OpenGLWindowGraphics(OpenGLWindowGraphicsAdapter adapter, Window window, bool vsync)
                 : base(adapter, window.Multisample)
             {
+                _adapter = adapter;
                 _window = window;
                 _vsync = vsync;
 
@@ -75,12 +102,40 @@ namespace Heirloom.Desktop
 
                 // Configure swap interval
                 Glfw.SetSwapInterval(_vsync ? 1 : 0);
+
+                // Append graphics context tracking list
+                lock (_adapter.Graphics) { _adapter.Graphics.Add(this); }
             }
 
             protected override void SwapBuffers()
             {
                 // Swap buffers (on the gl thread)
-                Invoke(() => Glfw.SwapBuffers(_window.Handle), false);
+                base.Invoke(() => Glfw.SwapBuffers(_window.Handle), false);
+            }
+
+            #region Invoke
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            internal new void Invoke(Action action, bool blocking = true)
+            {
+                base.Invoke(action, blocking);
+            }
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            internal new T Invoke<T>(Func<T> action)
+            {
+                return base.Invoke(action);
+            }
+
+            #endregion
+
+            protected override void Dispose(bool disposeManaged)
+            {
+                // Remove from context tracking list
+                lock (_adapter.Graphics) { _adapter.Graphics.Remove(this); }
+
+                // Dispose base class
+                base.Dispose(disposeManaged);
             }
         }
     }
