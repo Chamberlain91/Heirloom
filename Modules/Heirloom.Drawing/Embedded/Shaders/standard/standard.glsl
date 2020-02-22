bc7d529549cc89f2b0590ac547cc452e378c2e02
Christopher Chamberlain <chamberlain@live.ca>
2020-02-21 23:38:00 -0600

    Implemented linear sampling at shader level

--- Modules/Heirloom.Drawing/Embedded/Shaders/standard/standard.glsl
+++ Modules/Heirloom.Drawing/Embedded/Shaders/standard/standard.glsl
@@ -20,27 +20,83 @@ uniform Standard
 	mat2x3 uMatrix;
 };
 
-// Basically "redefine" texture calls to use atlas function
-#define texture(img, uv) \
-	texture(img, computeAtlasUV(uv, img ## _UVRect))
-
 // maps 'image space' to 'atlas space'
-vec2 computeAtlasUV(in vec2 uv, in vec4 uvRect) {
+vec2 wrap(in vec2 uv, in vec2 offset, in vec2 size, in vec4 rect) {
+	return mod((uv * rect.zw) + offset, rect.zw) + rect.xy;
+}
 
-	// Wrap UV coordinates
-	uv = mod(uv, 1.0);
+// nearest sampling (no interpolation)
+vec4 sampleAtlasNearest(sampler2D img, vec2 uv, vec4 rect)
+{
+	// Acquire image size
+	ivec2 size = textureSize(img, 0);
 
-	// Scale and translate into atlas space
-	uv = (uv * uvRect.zw) + uvRect.xy;
+	// Transform atlas rect to pixels space
+	rect = rect * vec4(size, size);
 
-	// If we have a scale height, we need to "y-flip"
-	if (uvRect.w < 0.0) {
-		uv.y += 1.0;
+	// Compute sampling positions (in pixel space)
+	// todo: wrap modes?
+	vec2 st = mod(uv * rect.zw, rect.zw) + rect.xy;
+
+	// We encode 'flip on y' in negative height atlas rectangle.
+	// So if we see this, we need to compensate for this.
+	if (rect.w < 0.0)
+	{
+		// todo: This is probably incorrect in the general case,
+		// but appears to make surfaces work as I intended.
+		st.y += size.y;
 	}
 
-	// Returns the mapped coordinate
-	return uv;
+	// Fetch texel
+	return texelFetch(img, ivec2(st), 0);
 }
+
+// linear sampling
+vec4 sampleAtlasLinear(sampler2D img, vec2 uv, vec4 rect)
+{
+	// Acquire image size
+	ivec2 size = textureSize(img, 0);
+
+	// Transform atlas rect to pixels space
+	rect = rect * vec4(size, size);
+
+	// Compute sampling positions (in pixel space)
+	// todo: wrap modes?
+	vec2 st00 = mod((uv * rect.zw) + vec2(0, 0), rect.zw) + rect.xy;
+	vec2 st10 = mod((uv * rect.zw) + vec2(1, 0), rect.zw) + rect.xy;
+	vec2 st01 = mod((uv * rect.zw) + vec2(0, 1), rect.zw) + rect.xy;
+	vec2 st11 = mod((uv * rect.zw) + vec2(1, 1), rect.zw) + rect.xy;
+
+	// Get fractional component
+	vec2 fst = fract(st00);
+
+	// We encode 'flip on y' in negative height atlas rectangle.
+	// So if we see this, we need to compensate for this.
+	if (rect.w < 0.0)
+	{
+		// todo: This is probably incorrect in the general case,
+		// but appears to make surfaces work as I intended.
+		st00.y += size.y;
+		st10.y += size.y;
+		st01.y += size.y;
+		st11.y += size.y;
+	}
+
+	// Fetch texels
+	vec4 t00 = texelFetch(img, ivec2(st00), 0);
+	vec4 t10 = texelFetch(img, ivec2(st10), 0);
+	vec4 t01 = texelFetch(img, ivec2(st01), 0);
+	vec4 t11 = texelFetch(img, ivec2(st11), 0);
+	
+	// Interpolate
+	vec4 t0 = mix(t00, t10, fst.x);
+	vec4 t1 = mix(t01, t11, fst.x);
+	return mix(t0, t1, fst.y);
+}
+
+// Cause replacement of texture() calls to use atlas function...
+#define texture(img, uv) \
+    sampleAtlasLinear(img, uv, img ## _UVRect)
 
 // computes the luminance of a color
 float luminance(vec3 rgb) {
