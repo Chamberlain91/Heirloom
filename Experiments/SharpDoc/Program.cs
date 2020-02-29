a659462868ddbea52db8de2b30cf14619e100fd4
Christopher Chamberlain <chamberlain@live.ca>
2020-02-29 03:37:09 -0600

    Better implemented generator

--- Experiments/SharpDoc/Program.cs
+++ Experiments/SharpDoc/Program.cs
@@ -8,34 +8,20 @@ namespace SharpDoc
     {
         private static void Main(string[] args)
         {
+            // todo: get from args and use Assembly.ReflectionOnlyLoad?
             var assembly = typeof(Graphics).Assembly;
 
             // Delete directory (if exists) and regenerate
-            var dirName = Path.GetFileNameWithoutExtension(assembly.Location);
-            if (Directory.Exists(dirName)) { Directory.Delete(dirName, true); }
-            Directory.CreateDirectory(dirName);
+            var dir = Path.GetFileNameWithoutExtension(assembly.Location);
+            if (Directory.Exists(dir)) { Directory.Delete(dir, true); }
+            Directory.CreateDirectory(dir);
 
-            // Emit files for each type
-            foreach (var type in assembly.ExportedTypes)
+            // Generate Markdown Documentation
+            var generator = new MarkdownGenerator();
+            foreach (var (path, text) in generator.Generate(assembly, dir))
             {
-                // Emit File Header
-                var markdown = $"{type.Namespace}\n";
-                markdown += $"{new string('-', type.Namespace.Length)}\n\n";
-
-                // Emit Type
-                markdown += MarkdownGenerator.GenerateMarkdown(type);
-
-                // Write to disk
-                var filePath = SanitizePath($"{type.Namespace}.{type.GetHumanName()}.md");
-                File.WriteAllText(Path.Join(dirName, filePath), markdown);
+                File.WriteAllText(path, text);
             }
-        }
-
-        private static string SanitizePath(string path)
-        {
-            path = path.Replace('<', '[');
-            path = path.Replace('>', ']');
-            return path;
         }
     }
 }
