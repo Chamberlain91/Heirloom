d8bf29a00ca435a9c1f768ec36542560a715429e
Christopher Chamberlain <chamberlain@live.ca>
2020-03-03 15:45:26 -0600

    Switched out for RenderLoop because something wasn't working...

--- Examples/Examples.Rasterizer/Program.cs
+++ Examples/Examples.Rasterizer/Program.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+
 using Heirloom.Desktop;
 using Heirloom.Drawing;
 using Heirloom.Math;
@@ -7,57 +8,58 @@ using Heirloom.Math.Extras;
 
 namespace Examples.CpuRasterizer
 {
-    class Program
+    internal class Program : RenderLoop
     {
-        public Window Window;
+        public readonly Window Window;
 
-        public Image Image;
+        public readonly Image Image;
 
-        public Program()
+        public Program(Window window)
+            : base(window.Graphics)
         {
-            // Create window
-            Window = new Window("CPU Rasterizer")
-            {
-                Size = (512, 512),
-                IsResizable = false
-            };
+            Window = window;
 
             // 
-            Image = new Image(Window.FramebufferSize / 2);
+            Image = new Image(Window.FramebufferSize);
             Image.Clear(Color.DarkGray);
 
             // Rasterize into image (CPU rendering)
             FillPixels(Rasterizer.Rectangle(Image.Width / 4, 0, Image.Height / 2, Image.Height), Color.Gray);
             FillPixels(Rasterizer.Circle(Image.Width / 2, Image.Height * 2 / 3, 96), Color.Pink);
             FillPixels(Rasterizer.Circle(Image.Width / 2, Image.Height / 2, 64), Color.Cyan);
-            FillPixels(Rasterizer.Circle(Image.Width / 2, Image.Height / 3, 32), Color.White);
+            FillPixels(Rasterizer.Circle(Image.Width / 2, Image.Height / 3, 48), Color.White);
         }
 
         private void FillPixels(IEnumerable<IntVector> positions, Color color)
         {
-            foreach (var co in positions)
+            Parallel.ForEach(positions, co =>
             {
                 if (co.X >= 0 && co.Y >= 0 && co.X < Image.Width && co.Y < Image.Height)
                 {
                     Image.SetPixel(co, color);
                 }
-            }
+            });
         }
 
-        private void Update()
+        protected override void Update(Graphics gfx, float dt)
         {
-            // Draw image to screen
-            Window.Graphics.ResetState();
             Window.Graphics.DrawImage(Image, new Rectangle(Vector.Zero, Window.Size));
-            Window.Graphics.RefreshScreen();
         }
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Application.Run(() =>
             {
-                var program = new Program();
-                program.Update(); //
+                // Create window
+                var window = new Window("CPU Rasterizer")
+                {
+                    Size = (512, 512),
+                    IsResizable = false
+                };
+
+                // 
+                var program = new Program(window);
+                program.Start();
             });
         }
     }
