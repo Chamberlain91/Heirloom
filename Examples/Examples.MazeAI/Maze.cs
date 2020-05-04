ddaf48414f36299c74b5f6691dc4408fed18074a
Christopher Chamberlain <chamberlain@live.ca>
2020-05-04 07:15:59 -0600

    Replaced graphics with something more suitable to the maze data

---
+++ Examples/Examples.MazeAI/Maze.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Heirloom;
+using Heirloom.Collections;
+
+namespace Examples.MazeAI
+{
+    public class Maze
+    {
+        public Maze()
+        {
+            Tiles = new Grid<int>(31, 31);
+            Tiles.Clear(-1);
+
+            // Generate graph
+            Graph = Generate();
+
+            // Render floors into tileset
+            foreach (var source in Graph.Vertices)
+            {
+                // For each place visible
+                foreach (var target in Graph.GetSuccessors(source))
+                {
+                    var aco = (1, 1) + (source * 2);
+                    var bco = (1, 1) + (target * 2);
+
+                    // Draw floors into tile map
+                    foreach (var m in Rasterizer.Line(aco, bco)) { Tiles[m] = 0; }
+                }
+            }
+
+            // Render walls
+            foreach (var co in GetCoordinates(31, 31))
+            {
+                if (Tiles[co] == 0) { continue; } // already a floor
+                else
+                {
+                    var north = co + (0, -1);
+                    var south = co + (0, +1);
+                    var west = co + (-1, 0);
+                    var east = co + (+1, 0);
+
+                    var face = Face.None;
+                    if ((Tiles.IsValidCoordinate(north) && Tiles[north] == 0) || co.Y == 0) { face |= Face.North; }
+                    if ((Tiles.IsValidCoordinate(south) && Tiles[south] == 0) || co.Y == 30) { face |= Face.South; }
+                    if ((Tiles.IsValidCoordinate(east) && Tiles[east] == 0) || co.X == 30) { face |= Face.East; }
+                    if ((Tiles.IsValidCoordinate(west) && Tiles[west] == 0) || co.X == 0) { face |= Face.West; }
+
+                    // 
+                    Tiles[co] = 1;
+
+                    if (face == Face.None) { Tiles[co] = 2; }
+                    else
+                    {
+                        // Vertical Wall
+                        if (face == (Face.East | Face.West)) { Tiles[co] = 3; }
+                        else if (face == (Face.East | Face.West | Face.North)) { Tiles[co] = 3; }
+                        else if (face == (Face.East | Face.West | Face.South)) { Tiles[co] = 8; }
+                        // Horizontal Wall
+                        else if (face == (Face.South | Face.North | Face.West)) { Tiles[co] = 1; }
+                        else if (face == (Face.South | Face.North | Face.East)) { Tiles[co] = 1; }
+                        else if (face == (Face.South | Face.North)) { Tiles[co] = 1; }
+                        else if (face == Face.South) { Tiles[co] = 1; }
+                        // Horizontal To Vertical (Cross)
+                        else if (face == Face.North) { Tiles[co] = 2; }
+                        else if (face == Face.None) { Tiles[co] = 2; }
+                        // Upper Right Corner
+                        else if (face == Face.East) { Tiles[co] = 4; }
+                        else if (face == (Face.East | Face.North)) { Tiles[co] = 4; }
+                        // Upper Left Corner
+                        else if (face == Face.West) { Tiles[co] = 5; }
+                        else if (face == (Face.West | Face.North)) { Tiles[co] = 5; }
+                        // Bottom Right Corner
+                        else if (face == (Face.South | Face.East)) { Tiles[co] = 6; }
+                        // Bottom Left Corner
+                        else if (face == (Face.South | Face.West)) { Tiles[co] = 7; }
+                        // Trash Image
+                        else { Tiles[co] = -1; }
+                    }
+                }
+            }
+        }
+
+        public Grid<int> Tiles { get; }
+
+        public UndirectedGraph<IntVector> Graph { get; }
+
+        private UndirectedGraph<IntVector> Generate()
+        {
+            var graph = new UndirectedGraph<IntVector>();
+
+            var coords = GetCoordinates(15, 15).ToArray();
+            coords.Randomize();
+
+            // Insert each grid position
+            foreach (var co in coords)
+            {
+                graph.Add(co);
+            }
+
+            // Connect edges to each grid position
+            foreach (var co in graph.Vertices)
+            {
+                // Connect to neighbors with random weight
+                foreach (var nco in Tiles.GetNeighborCoordinates(co, GridNeighborType.Axis))
+                {
+                    if (graph.Contains(nco) && !graph.IsConnected(co, nco))
+                    {
+                        // Add connection with random path weight
+                        graph.Connect(co, nco, Calc.Random.NextFloat());
+                    }
+                }
+            }
+
+            return graph.FindMinimumSpanningTree();
+        }
+
+        public static IEnumerable<IntVector> GetCoordinates(int w, int h)
+        {
+            for (var y = 0; y < h; y++)
+            {
+                for (var x = 0; x < w; x++)
+                {
+                    yield return new IntVector(x, y);
+                }
+            }
+        }
+
+        [Flags]
+        private enum Face
+        {
+            None = 0,
+
+            North = 1 << 1,
+            South = 1 << 2,
+            East = 1 << 3,
+            West = 1 << 4
+        }
+    }
+}
--- Examples/Examples.MazeAI/Program.cs
+++ Examples/Examples.MazeAI/Program.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 using Heirloom;
-using Heirloom.Collections;
 using Heirloom.Desktop;
 
 namespace Examples.MazeAI
@@ -37,131 +33,33 @@ namespace Examples.MazeAI
             // Draw Tile Grid
             foreach (var co in Maze.GetCoordinates(31, 31))
             {
-                var image = Assets.GetImage(Maze.Tiles[co]);
-                gfx.DrawImage(image, co * 8);
-            }
-
-            // Draw Graph
-            foreach (var source in Maze.Graph.Vertices)
-            {
-                foreach (var target in Maze.Graph.GetSuccessors(source))
+                var tileIndex = Maze.Tiles[co];
+                if (tileIndex >= 0)
                 {
-                    var a = (12, 12) + source * 16F;
-                    var b = (12, 12) + target * 16F;
-                    gfx.Color = Color.FromHSV(0F, 1F, Maze.Graph.GetWeight(source, target));
-                    gfx.DrawLine(a, b);
+                    var image = Assets.GetImage(tileIndex);
+                    gfx.DrawImage(image, co * 8);
                 }
-            }
-        }
-    }
-
-    public class Maze
-    {
-        public Maze()
-        {
-            Tiles = new Grid<int>(31, 31);
-            Tiles.Clear(12);
-
-            // Generate graph
-            Graph = Generate();
-
-            // Render floors into tileset
-            foreach (var source in Graph.Vertices)
-            {
-                // For each place visible
-                foreach (var target in Graph.GetSuccessors(source))
+                else
                 {
-                    var aco = (1, 1) + (source * 2);
-                    var bco = (1, 1) + (target * 2);
-
-                    // Draw edges into tile map
-                    foreach (var m in Rasterizer.Line(aco, bco)) { Tiles[m] = 11; }
+                    // Bad!
+                    gfx.PushState();
+                    gfx.Color = Color.Orange;
+                    gfx.DrawRect((co * 8, (8, 8)));
+                    gfx.PopState();
                 }
             }
 
-            //// Render walls
-            //foreach (var co in GetCoordinates(31, 31))
+            //// Draw Graph
+            //foreach (var source in Maze.Graph.Vertices)
             //{
-            //    if (Tiles[co] == 11) { continue; } // already a floor
-            //    else
+            //    foreach (var target in Maze.Graph.GetSuccessors(source))
             //    {
-            //        var south = co + (0, +1);
-            //        var west = co + (-1, 0);
-            //        var east = co + (+1, 0);
-
-            //        var face = Face.None;
-            //        if (Tiles.IsValidCoordinate(south) && Tiles[south] == 11) { face |= Face.South; }
-            //        if (Tiles.IsValidCoordinate(east) && Tiles[east] == 11) { face |= Face.East; }
-            //        if (Tiles.IsValidCoordinate(west) && Tiles[west] == 11) { face |= Face.West; }
-            //        if (co.Y == 30) { face |= Face.South; }
-
-            //        Tiles[co] = 27;
-
-            //        if (face == Face.None) { Tiles[co] = 60; }
-            //        else
-            //        {
-            //            if (face == (Face.South | Face.East)) { Tiles[co] = 83; }
-            //            else if (face == (Face.South | Face.West)) { Tiles[co] = 30; }
-            //            else if (face == Face.South) { Tiles[co] = 1; }
-            //            else { Tiles[co] = 13; }
-            //        }
+            //        var a = (12, 12) + source * 16F;
+            //        var b = (12, 12) + target * 16F;
+            //        gfx.Color = Color.FromHSV(0F, 1F, Maze.Graph.GetWeight(source, target));
+            //        gfx.DrawLine(a, b);
             //    }
             //}
-        }
-
-        public Grid<int> Tiles { get; }
-
-        public UndirectedGraph<IntVector> Graph { get; }
-
-        private UndirectedGraph<IntVector> Generate()
-        {
-            var graph = new UndirectedGraph<IntVector>();
-
-            var coords = GetCoordinates(15, 15).ToArray();
-            coords.Randomize();
-
-            // Insert each grid position
-            foreach (var co in coords)
-            {
-                graph.Add(co);
-            }
-
-            // Connect edges to each grid position
-            foreach (var co in graph.Vertices)
-            {
-                // Connect to neighbors with random weight
-                foreach (var nco in Tiles.GetNeighborCoordinates(co, GridNeighborType.Axis))
-                {
-                    if (graph.Contains(nco) && !graph.IsConnected(co, nco))
-                    {
-                        // Add connection with random path weight
-                        graph.Connect(co, nco, Calc.Random.NextFloat());
-                    }
-                }
-            }
-
-            return graph.FindMinimumSpanningTree();
-        }
-
-        public static IEnumerable<IntVector> GetCoordinates(int w, int h)
-        {
-            for (var y = 0; y < h; y++)
-            {
-                for (var x = 0; x < w; x++)
-                {
-                    yield return new IntVector(x, y);
-                }
-            }
-        }
-
-        [Flags]
-        private enum Face
-        {
-            None = 0,
-
-            South = 1 << 1,
-            East = 1 << 2,
-            West = 1 << 3
         }
     }
 }
