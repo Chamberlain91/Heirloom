4d3597d63487a5f70fb76591a08fcb1c96f49577
Christopher Chamberlain <chamberlain@live.ca>
2020-07-03 06:46:02 -0600

    Implemented a Gradient type.

---
+++ Modules/Heirloom.Core/Core/Drawing/GradientMode.cs
@@ -0,0 +1,18 @@
+﻿namespace Heirloom
+{
+    /// <summary>
+    /// Configures how a <see cref="Gradient"/> will interpolate colors.
+    /// </summary>
+    public enum GradientMode
+    {
+        /// <summary>
+        /// A simple linear interpolation of RGB components.
+        /// </summary>
+        RGB,
+
+        /// <summary>
+        /// Converts interpolated colors into CIELab space for interpolation.
+        /// </summary>
+        CIELab
+    };
+}
---
+++ Modules/Heirloom.Core/Core/Drawing/GradientStop.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Heirloom
+{
+    /// <summary>
+    /// Represents a specific gradient stop (color key).
+    /// </summary>
+    public readonly struct GradientStop
+    {
+        /// <summary>
+        /// The normalized time of this stop.
+        /// </summary>
+        public readonly float Time;
+
+        /// <summary>
+        /// The color of this stop.
+        /// </summary>
+        public readonly Color Color;
+
+        internal GradientStop(float time, Color color)
+        {
+            Time = time;
+            Color = color;
+        }
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj)
+        {
+            return obj is GradientStop other &&
+                   Time == other.Time &&
+                   Color.Equals(other.Color);
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Time, Color);
+        }
+
+        /// <inheritdoc/>
+        public void Deconstruct(out float time, out Color color)
+        {
+            time = Time;
+            color = Color;
+        }
+    }
+}
